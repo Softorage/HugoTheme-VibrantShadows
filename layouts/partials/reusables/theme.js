f22f505b39b8d236eafd3135f5989b4ddcd50338
Sanmay Joshi <sanmaycjoshi@gmail.com>
2023-11-25 20:42:42 +0530

    Following changes: 1. Updated thumbnail 2. Updated theme toggle to fix wrong status on toggle than current theme

--- layouts/partials/reusables/theme.js
+++ layouts/partials/reusables/theme.js
@@ -14,7 +14,7 @@ Example: # prints hello
   hello
 {{ end }}
  -->*/}}
- 
+
 {{ $cssTheme := "dark" }}
 
 {{ if site.Params.theme.light.default }}
@@ -22,6 +22,7 @@ Example: # prints hello
 {{ end }}
 
 // Based on https://css-tricks.com/a-complete-guide-to-dark-mode-on-the-web/
+
 // Select the button
 const themeToggle = document.querySelector(".theme-toggle");
 const themeToggleCheckbox = document.querySelector("#theme_toggle");
@@ -29,42 +30,70 @@ const themeToggleCheckbox = document.querySelector("#theme_toggle");
 const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
 // Get the user's theme preference from local storage, if it's available
 let currentTheme = localStorage.getItem("theme");
+// 
+let bodyClass = document.body.classList;
+
+// User's theme preference from local storage has highest priority,
+// then system preference set by user at OS level,
+// then theme set by website administrator (you) in $cssTheme
+
+// This is the simple version of the code that may be slightly inefficient but clear and easy to understand 
+if (currentTheme === "dark") {
+  darkTheme();
+} else if (currentTheme === "light") {
+  lightTheme();
+} else if (prefersDarkScheme.matches) {
+  darkTheme();
+} else if ("{{ $cssTheme }}" === "dark") {
+  darkTheme();
+} else if ("{{ $cssTheme }}" === "light") {
+  lightTheme();
+}
+// function definitions
+const darkTheme = function () {
+  bodyClass.remove("light"); // remove class 'light'
+  bodyClass.add("dark"); // add class 'dark'
+  themeToggleCheckbox.checked = true;
+}
+const lightTheme = function () {
+  bodyClass.remove("dark"); // remove class 'dark'
+  bodyClass.add("light"); // add class 'light'
+  themeToggleCheckbox.checked = false;
+}
+
+/*
+// below code doesn't work as expected. toggle shows dark mode when light mode is set and vice versa.
 
 // We don't want to toggle the class of body tag (dark or light), if the theme as per localStorage and theme as per default CSS link tag matches
 // since we are checking for system preference for dark theme in the same if conditional that checks for theme info stored in localStorage, it is likely to be effecient. As such no separate media query (prefers-color-scheme) has been specified in CSS.
 // If the user's preference in localStorage is dark and default theme isn't dark, or
 // If user has set system preference to dark and default theme is set to light and user has no preference in localStorage (if user has a preference stored in localStorage, then such preference shall override the system preference)
-if ((currentTheme == "dark" && "{{ $cssTheme }}" != "dark") || (prefersDarkScheme.matches && "{{ $cssTheme }}" == "light" && !currentTheme)) {
+if ((currentTheme == "dark" && "{{ $cssTheme }}" != "dark") || (!currentTheme && prefersDarkScheme.matches && "{{ $cssTheme }}" == "light")) {
   // This if conditional executes only if the body tag has class 'light'. so we need to toggle class 'light' to remove it and class 'dark' to add it
-  document.body.classList.toggle("light"); // remove class 'light'
-  document.body.classList.toggle("dark"); // add class 'dark'
+  bodyClass.remove("light"); // remove class 'light'
+  bodyClass.add("dark"); // add class 'dark'
+  themeToggleCheckbox.checked = true;
 // Otherwise, if the user's preference in localStorage is light and default theme isn't light
 } else if (currentTheme == "light" && "{{ $cssTheme }}" != "light") {
   // This if conditional executes only if the body tag has class 'dark'. So we need to toggle class 'dark' to remove it and class 'light' to add it
-  document.body.classList.toggle("dark"); // remove class 'dark'
-  document.body.classList.toggle("light"); // add class 'light'
-}
-
-// Make the themetoggle match the current theme
-if (currentTheme == "dark") {
-  themeToggleCheckbox.checked = true;
-} else {
+  bodyClass.remove("dark"); // remove class 'dark'
+  bodyClass.add("light"); // add class 'light'
   themeToggleCheckbox.checked = false;
 }
+*/
 
 // Listen for a click on the button 
 themeToggle.addEventListener("click", function() {
-  document.body.classList.toggle("light");
-  document.body.classList.toggle("dark"); 
+  bodyClass.toggle("light");
+  bodyClass.toggle("dark"); 
   // Update the currentTheme variable to save the new theme in localStorage
-  if (currentTheme == "dark") {
+  if (currentTheme === "dark") {
     currentTheme = "light";
+    //themeToggleCheckbox.checked = false;
   } else {
     currentTheme = "dark";
+    //themeToggleCheckbox.checked = true;
   }
-  // Finally, let's save the current preference to localStorage to keep using it
+  // Finally, let's save the current preference to localStorage to keep using it even on a different page
   localStorage.setItem("theme", currentTheme);
 });
-
-
-
